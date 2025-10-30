44e843f801f8a543ba07abb8c8b85e972142e37a
TBG AI enrichment <laarmann@ylab.es>
2025-10-30 10:28:52 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- tbg_nodes.py
+++ tbg_nodes.py
@@ -1,6 +1,6 @@
 """MIT License
 
-Copyright (c) [2025] [TBG]
+Copyright (c) [2025] [TBG Tobias Laarmann]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -668,4 +668,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "LogSigmaStepSamplerNode":"LogSigmaStepSamplerNode",
     "TBG_FluxKontextStabilizer":"TBG_FluxKontextStabilizer",
 }
+
 
